c2603c14e8e8eaaaf232c544fe602ea8858ca7fd
arinmsn <arinmsn@gmail.com>
2019-09-11 04:48:25 -0700

    Updated JS file to reflect the event handler etc. for the modal/backdrop

--- Responsive-Site/shared.js
+++ Responsive-Site/shared.js
@@ -1,11 +1,32 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
 // This will return a Node list
 var selectPlanButton = document.querySelectorAll(".plan button");
 // console.dir(backdrop);
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
 for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener("click", function() {
     modal.style.display = "block";
     backdrop.style.display = "block";
   });
 }
+
+backdrop.addEventListener("click", function() {
+  mobileNav.style.display = "none";
+  closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
