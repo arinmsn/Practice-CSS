3ceed7c4fac017216022fec0ad872b8f5adf9b53
arinmsn <arinmsn@gmail.com>
2019-09-13 20:01:13 -0700

    Added nav to other pages

--- Responsive-Site/shared.js
+++ Responsive-Site/shared.js
@@ -2,13 +2,13 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 // This will return a Node list
-var selectPlanButton = document.querySelectorAll(".plan button");
+var selectPlanButtons = document.querySelectorAll(".plan button");
 // console.dir(backdrop);
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
-for (var i = 0; i < selectPlanButton.length; i++) {
-  selectPlanButton[i].addEventListener("click", function() {
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
     modal.style.display = "block";
     backdrop.style.display = "block";
   });
